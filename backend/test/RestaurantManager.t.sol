48ade22fda08ea7e913eceef152b1cd6cb690d13
Kevin Sheth <kevinsheth25@gmail.com>
2023-03-30 22:55:21 -0400

    Wrote first tests for RM

--- backend/test/RestaurantManager.t.sol
+++ backend/test/RestaurantManager.t.sol
@@ -7,5 +7,51 @@ import "../src/RestaurantManager.sol";
 contract RestaurantManagerTest is Test {
     RestaurantManager restaurantManager;
 
-    function setUp() public {}
+    function setUp() public {
+        restaurantManager = new RestaurantManager();
+    }
+
+    function test_RegisterRestaurant() public {
+        restaurantManager.registerRestaurant(
+            "Hungry Hippos",
+            "123 Main St, New York, NY 10001"
+        );
+
+        RestaurantManager.Restaurant memory res = restaurantManager
+            .getRestaurant(0);
+
+        assertEq(
+            res.name,
+            "Hungry Hippos",
+            "Restaurant name should be Hungry Hippos"
+        );
+
+        assertEq(
+            res.location,
+            "123 Main St, New York, NY 10001",
+            "Restaurant location should be 123 Main St, New York, NY 10001"
+        );
+
+        assertEq(
+            res.owner,
+            address(this),
+            "Restaurant owner should be the address of the test contract"
+        );
+    }
+
+    function test_GetAllRestaurants() public {
+        restaurantManager.registerRestaurant(
+            "Hungry Hippos",
+            "123 Main St, New York, NY 10001"
+        );
+        restaurantManager.registerRestaurant(
+            "Meaty Meatballs",
+            "125 Main St, New York, NY 10001"
+        );
+
+        RestaurantManager.Restaurant[] memory rests = restaurantManager
+            .getAllRestaurants();
+
+        assertEq(rests.length, 2, "There should be one restaurants");
+    }
 }
