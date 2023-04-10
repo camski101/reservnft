943c0383a583026c06a4c430b747978832e41c3f
Kevin Sheth <kevinsheth25@gmail.com>
2023-04-10 19:58:44 -0400

    Clean up RegisterRestaurant

--- frontend/components/RegisterRestaurant.jsx
+++ frontend/components/RegisterRestaurant.jsx
@@ -13,7 +13,7 @@ export default function RegisterRestaurant({ onDataChange }) {
     // State for form input values
     const [restaurantName, setRestaurantName] = useState("")
     const [restaurantBusinessAddress, setRestaurantBusinessAddress] = useState("")
-    const [transactionSuccess, setTransactionSuccess] = useState(true)
+    const [buttonLoading, setButtonLoading] = useState(false)
 
     const {
         runContractFunction: registerRestaurant,
@@ -32,80 +32,85 @@ export default function RegisterRestaurant({ onDataChange }) {
 
     // Function to handle form submission
     const handleSubmit = async (e) => {
-        setTransactionSuccess(false)
+        setButtonLoading(true)
         e.preventDefault()
         await registerRestaurant({
             onSuccess: (tx) => {
                 handleSuccess(tx)
             },
             onError: (error) => {
+                handleError(error)
                 console.log(error)
             },
+        })
+    }
+
+    const handleNewNotification = (type, message, title, tx) => {
+        dispatch({
+            type: type,
+            message: message,
+            title: title,
+            position: "topR",
         })
     }
 
     const handleSuccess = async (tx) => {
         try {
             await tx.wait(1)
-            handleNewNotification(tx)
-
+            handleNewNotification("info", "Transaction Complete!", "Transaction Notification", tx)
             setRestaurantName("")
             setRestaurantBusinessAddress("")
-            setTransactionSuccess(true)
+            setButtonLoading(false)
             onDataChange()
         } catch (error) {
             console.log(error)
         }
     }
 
-    const handleNewNotification = () => {
-        dispatch({
-            type: "info",
-            message: "Transaction Complete!",
-            title: "Transaction Notification",
-            position: "topR",
-        })
+    const handleError = (error) => {
+        handleNewNotification("error", error.message, "Transaction Notification")
+        setButtonLoading(false)
     }
 
     if (!rmAddress) {
-        return null
+        return
+    } else {
+        return (
+            <div className="p-5 border-1">
+                <h1 className="pb-4 font-bold text-3xl">Register Restaurant</h1>
+
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <input
+                        type="text"
+                        placeholder="Restaurant Name"
+                        value={restaurantName}
+                        onChange={(e) => setRestaurantName(e.target.value)}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                        disabled={buttonLoading}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Restaurant Business Address"
+                        value={restaurantBusinessAddress}
+                        onChange={(e) => setRestaurantBusinessAddress(e.target.value)}
+                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                        disabled={buttonLoading}
+                    />
+                    <Button
+                        theme="primary"
+                        text={
+                            buttonLoading ? (
+                                <Loading size={20} spinnerColor="#ffffff" spinnerType="wave" />
+                            ) : (
+                                "Register Restaurant"
+                            )
+                        }
+                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+                        type="submit"
+                        disabled={!restaurantName || !restaurantBusinessAddress}
+                    />
+                </form>
+            </div>
+        )
     }
-
-    return (
-        <div className="p-5 border-1">
-            <h1 className="pb-4 font-bold text-3xl">Register Restaurant</h1>
-
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <input
-                    type="text"
-                    placeholder="Restaurant Name"
-                    value={restaurantName}
-                    onChange={(e) => setRestaurantName(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                    disabled={!transactionSuccess}
-                />
-                <input
-                    type="text"
-                    placeholder="Restaurant Business Address"
-                    value={restaurantBusinessAddress}
-                    onChange={(e) => setRestaurantBusinessAddress(e.target.value)}
-                    className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                    disabled={!transactionSuccess}
-                />
-                <Button
-                    theme="primary"
-                    text={
-                        isLoading || !transactionSuccess ? (
-                            <Loading size={20} spinnerColor="#ffffff" spinnerType="wave" />
-                        ) : (
-                            "Register Restaurant"
-                        )
-                    }
-                    className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
-                    type="submit"
-                    disabled={!transactionSuccess || !restaurantName || !restaurantBusinessAddress}
-                />
-            </form>
-        </div>
-    )
 }
