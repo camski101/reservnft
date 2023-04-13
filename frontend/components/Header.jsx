2245ca9f0e7a1245b3451d240072896bc7e29588
Kevin Sheth <kevinsheth25@gmail.com>
2023-04-13 10:14:07 -0400

    Revert I think

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,20 +1,21 @@
 import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
 export default function Header() {
     return (
         <nav className="p-5 flex flex-row justify-between items-center">
             <h1 className="py-4 px-4 font-bold text-3xl">
-                <a href="#/" className="text-current">
+                <Link href="/" className="text-current">
                     ReservNFT
-                </a>
+                </Link>
             </h1>
             <div className="flex flex-row items-center">
-                <a href="#/" className="mr-4 p-6">
+                <Link href="/" className="mr-4 p-6">
                     Home
-                </a>
-                <a href="#/restaurants" className="mr-4 p-6">
+                </Link>
+                <Link href="/restaurants" className="mr-4 p-6">
                     Restaurants
-                </a>
+                </Link>
                 <ConnectButton moralisAuth={false} />
             </div>
         </nav>
