f98b8412c41cfb0c3d9ad72fd737048d9c6d3fe5
camski101 <57109023+camski101@users.noreply.github.com>
2023-12-18 03:41:09 -0500

    Updated RM test covering related royalty logic
    SetUp: The createSampleDrop function now includes royalty parameters (BPS & receiver). The function now returns a drop’s details with this royalty info.
    
    Updated Test:
    I modified the test_CreateDropWithHourlyWindows to now include royalties; name is now test_CreateDropWithHourlyWindowsAndRoyalties.
    
    - This test function uses the createSampleDrop to create a sample drop with royalty data, and then retrieves those drop details with the royalty info.
    - I added two assertions to check to check that the rate (basis points) of the royalty and the receiver’s address match.

---
+++ backend/test/UpdatedRestaurantManager.t.sol
@@ -0,0 +1,294 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/RestaurantManager.sol";
+import "../interfaces/IReservNFT.sol";
+import "./mocks/MockReservNFT.sol";
+
+contract RestaurantManagerTest is Test {
+    RestaurantManager restaurantManager;
+    MockReservNFT mockReservNFT;
+
+    function setUp() public {
+        restaurantManager = new RestaurantManager();
+        mockReservNFT = new MockReservNFT();
+
+        // Set up the addresses after deploying both contracts
+        restaurantManager.setReservNFTAddress(address(mockReservNFT));
+        mockReservNFT.setRestaurantManagerAddress(address(restaurantManager));
+    }
+
+    function registerSampleRestaurant() internal returns (uint256) {
+        restaurantManager.registerRestaurant(
+            "Hungry Hippos",
+            "123 Main St, New York, NY 10001"
+        );
+    }
+
+    function createSampleDrop()
+        internal
+        returns (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow,
+            uint16 royaltiesBps, //adding royalty parameters
+            address royaltiesReceiver
+        )
+    {
+        uint64 startDate = 1700000000;
+        uint64 endDate = 1700003600;
+        uint32 dailyStartTime = 28800; // 8:00 am in seconds
+        uint32 dailyEndTime = 79200; // 10:00 pm in seconds
+        uint32 windowDuration = 3600; // hourly windows in seconds
+        uint16 reservationsPerWindow = 10;
+        uint16 royaltiesBps = 100; // 1% royalty
+        address royaltiesReceiver = address(this); //using the test contract address for now
+
+        restaurantManager.createDrop(
+            0,
+            100,
+            startDate,
+            endDate,
+            dailyStartTime,
+            dailyEndTime,
+            windowDuration,
+            reservationsPerWindow,
+            royaltiesBps,
+            royaltiesReceiver
+        );
+
+        return (
+            startDate,
+            endDate,
+            dailyStartTime,
+            dailyEndTime,
+            windowDuration,
+            reservationsPerWindow,
+            royaltiesBps,
+            royaltiesReceiver
+        );
+    }
+
+    function test_RegisterRestaurant() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        RestaurantManager.Restaurant memory res = restaurantManager
+            .getRestaurant(restaurantId);
+
+        assertEq(
+            res.name,
+            "Hungry Hippos",
+            "Restaurant name should be Hungry Hippos"
+        );
+
+        assertEq(
+            res.businessAddress,
+            "123 Main St, New York, NY 10001",
+            "Restaurant location should be 123 Main St, New York, NY 10001"
+        );
+
+        assertEq(
+            res.owner,
+            address(this),
+            "Restaurant owner should be the address of the test contract"
+        );
+    }
+
+    function test_SetRestaurantIsActive() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        restaurantManager.setRestaurantIsActive(0, false);
+
+        RestaurantManager.Restaurant memory res = restaurantManager
+            .getRestaurant(restaurantId);
+
+        assertEq(res.isActive, false, "Restaurant should be inactive");
+    }
+
+    function test_RevertWhen_SetIsActiveByNonOwner() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        vm.prank(address(0x1234));
+
+        vm.expectRevert(RestaurantManager__Unauthorized.selector);
+
+        restaurantManager.setRestaurantIsActive(restaurantId, false);
+    }
+
+    function test_CreateDropWithHourlyWindowsAndRoyalties() public {
+        uint256 restaurantId = registerSampleRestaurant();
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow,
+            uint16 royaltiesBps,    //added royalty parameters in the test
+            address royaltiesReceiver
+        ) = createSampleDrop();
+
+        RestaurantManager.Drop memory drop = restaurantManager.getDrop(
+            restaurantId
+        );
+
+        // New assertions for royalty data
+        assertEq(drop.royaltiesBps, royaltiesBps, "Royalties basis points should match");
+        assertEq(drop.royaltiesReceiver, royaltiesReceiver, "Royalties receiver should match");
+
+        assertEq(drop.startDate, startDate, "Start date should match");
+        assertEq(drop.endDate, endDate, "End date should match");
+
+        assertEq(
+            drop.dailyStartTime,
+            dailyStartTime,
+            "Daily start time should match"
+        );
+        assertEq(
+            drop.dailyEndTime,
+            dailyEndTime,
+            "Daily end time should match"
+        );
+        assertEq(
+            drop.windowDuration,
+            windowDuration,
+            "Window duration should match"
+        );
+        assertEq(
+            drop.reservationsPerWindow,
+            reservationsPerWindow,
+            "Reservations per window should match"
+        );
+    }
+
+    function test_RevertWhen_CreateDropByNonOwner() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        vm.prank(address(0x1234));
+        vm.expectRevert(RestaurantManager__Unauthorized.selector);
+
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow
+        ) = createSampleDrop();
+    }
+
+    function test_SetDropIsActive() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow
+        ) = createSampleDrop();
+
+        restaurantManager.setDropIsActive(0, false);
+
+        RestaurantManager.Drop memory drop = restaurantManager.getDrop(0);
+
+        assertFalse(drop.isActive, "Drop should be inactive");
+    }
+
+    function test_RevertWhen_SetDropIsActiveByNonOwner() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow
+        ) = createSampleDrop();
+
+        vm.prank(address(0x1234));
+        vm.expectRevert(RestaurantManager__Unauthorized.selector);
+
+        restaurantManager.setDropIsActive(0, false);
+    }
+
+    function test_GetRestaurantDropCount() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow
+        ) = createSampleDrop();
+        uint256 dropCount = restaurantManager.getRestaurantDropCount(0);
+        assertEq(
+            dropCount,
+            1,
+            "There should be one drop associated with the restaurant"
+        );
+    }
+
+    function test_GetDropIDsByRestaurant() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        (
+            uint64 startDate,
+            uint64 endDate,
+            uint32 dailyStartTime,
+            uint32 dailyEndTime,
+            uint32 windowDuration,
+            uint16 reservationsPerWindow
+        ) = createSampleDrop();
+
+        uint256[] memory dropIDs = restaurantManager.getDropIDsByRestaurant(0);
+        assertEq(
+            dropIDs.length,
+            1,
+            "There should be one drop associated with the restaurant"
+        );
+        assertEq(dropIDs[0], 0, "The drop ID should be 0");
+    }
+
+    function test_RevertWhen_GetDropWithInvalidID() public {
+        vm.expectRevert(RestaurantManager__DropDoesNotExist.selector);
+        restaurantManager.getDrop(0);
+    }
+
+    function test_RevertWhen_GetRestaurantWithInvalidID() public {
+        vm.expectRevert(RestaurantManager__RestaurantDoesNotExist.selector);
+        restaurantManager.getRestaurant(0);
+    }
+
+    function test_RevertWhen_InvalidDropDates() public {
+        uint256 restaurantId = registerSampleRestaurant();
+
+        uint64 startDate = 1700000000;
+        uint64 endDate = 1700000000;
+        uint32 dailyStartTime = 28800; // 8:00 am in seconds
+        uint32 dailyEndTime = 79200; // 10:00 pm in seconds
+        uint32 windowDuration = 3600; // hourly windows in seconds
+        uint16 reservationsPerWindow = 10;
+
+        vm.expectRevert(RestaurantManager__InvalidDropDates.selector);
+
+        restaurantManager.createDrop(
+            0,
+            100,
+            startDate,
+            endDate,
+            dailyStartTime,
+            dailyEndTime,
+            windowDuration,
+            reservationsPerWindow
+        );
+    }
+}
