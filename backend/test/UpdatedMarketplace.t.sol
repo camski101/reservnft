940430d075c4d94efc2eabcfd342e70e1021444c
camski101 <57109023+camski101@users.noreply.github.com>
2023-12-18 03:44:53 -0500

    Updated Marketplace test to cover royalty
    SetUp: I updated the creation of a drop here to include and return the two royalty parameters, which is later leveraged in the test when an NFT is created using that drop logic. 
    
    Test: I then added a new test function called ‘test_BuyReservationWithRoyaltyPayment’ to test the royalty in an end-to-end transaction, and I hard-code the tokenID and sales price for the example. This function covers the process of 1) listing the NFT, 2) recording balances of the seller and royalty receiver (restaurant) before the sale, 3) simulating a purchase of the NFT, 4) calculating the expected royalty, and then assertions that check the royalty was processed right. The two assertions check:
    1. The royalty receiver’s balance has increased by the amount of the royalty
    2. The seller’s balance has increased the right amount, minus the royalty

---
+++ backend/test/UpdatedMarketplace.t.sol
@@ -0,0 +1,332 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "../src/Marketplace.sol";
+import "../src/ReservNFT.sol";
+import "../src/RestaurantManager.sol";
+
+contract MarketplaceTest is Test {
+    Marketplace marketplace;
+    ReservNFT reservNFT;
+    RestaurantManager restaurantManager;
+
+    function setUp() public {
+        reservNFT = new ReservNFT();
+        restaurantManager = new RestaurantManager();
+
+        marketplace = new Marketplace();
+
+        // Set up the addresses after deploying both contracts
+        restaurantManager.setReservNFTAddress(address(reservNFT));
+        reservNFT.setRestaurantManagerAddress(address(restaurantManager));
+        marketplace.setReservNFTAddress(address(reservNFT));
+
+        // Register a restaurant
+        restaurantManager.registerRestaurant(
+            "Test Restaurant",
+            "123 Main St, New York, NY 10001"
+        );
+        uint64 startDate = 1672560000; // January 1, 2023, 8:00 AM UTC
+        uint64 endDate = 1704060000; // December 31, 2023 10:00 PM UTC
+        uint32 dailyStartTime = 28800; // 8:00 am in seconds
+        uint32 dailyEndTime = 79200; // 10:00 pm in seconds
+        uint32 windowDuration = 3600; // hourly windows in seconds
+        uint16 reservationsPerWindow = 10;
+        uint16 royaltiesBps = 100; // added a 1% royalty
+        address royaltiesReceiver = address(this); // test contract address for meantime
+
+        restaurantManager.createDrop(
+            0,
+            0.01 ether,
+            startDate,
+            endDate,
+            dailyStartTime,
+            dailyEndTime,
+            windowDuration,
+            reservationsPerWindow,
+            royaltiesBps,
+            royaltiesReceiver
+        );
+    }
+
+    fallback() external payable {}
+
+    function _createReservNFT(
+        uint256 dropId,
+        uint256 reservationTimestamp,
+        uint256 value
+    ) internal returns (uint256) {
+        return
+            reservNFT.createReservNFT{value: value}(
+                dropId,
+                reservationTimestamp
+            );
+    }
+
+    function test_BuyReservationWithRoyaltyPayment() public payable {
+    uint256 tokenId = 1; 
+    uint256 salePrice = 0.02 ether;
+
+    // List the NFT for sale
+    marketplace.listReservation(tokenId, salePrice);
+
+    // Record balances before the sale
+    uint256 royaltyReceiverInitialBalance = address(royaltiesReceiver).balance;
+    uint256 sellerInitialBalance = address(this).balance;
+
+    // Simulate a buyer purchasing the NFT
+    marketplace.buyReservation{value: salePrice}(tokenId);
+
+    // Calculate expected royalty
+    uint256 expectedRoyalty = salePrice * royaltiesBps / 10000;
+
+    // Assertations to check that the royalty payment was correctly processed
+    assertEq(address(royaltiesReceiver).balance, royaltyReceiverInitialBalance + expectedRoyalty, "Royalty receiver should receive the correct royalty amount");
+    assertEq(address(this).balance, sellerInitialBalance + salePrice - expectedRoyalty, "Seller should receive sale price minus royalty");
+}
+
+    function test_listReservation() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400; // January 10, 2023 21:00:00 UTC
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        // Approve Marketplace to manage the token
+        reservNFT.approve(address(marketplace), tokenId);
+
+        // List the reservation
+        uint256 price = 0.02 ether;
+        marketplace.listReservation(tokenId, price);
+
+        // Check listing data
+        Marketplace.Listing memory listing = marketplace.getReservationListing(
+            tokenId
+        );
+        assertEq(listing.seller, address(this));
+        assertEq(listing.price, price);
+    }
+
+    function test_buyReservation() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400; // January 10, 2023 21:00:00 UTC
+
+        address alice = vm.addr(1);
+
+        payable(alice).transfer(5 ether);
+
+        vm.prank(alice);
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        address bob = vm.addr(2); // Add a new address for the buyer
+        payable(bob).transfer(5 ether);
+        // Transfer the token to Alice
+
+        // Approve Marketplace to manage the token
+        vm.prank(alice);
+        reservNFT.approve(address(marketplace), tokenId);
+
+        // List the reservation
+        uint256 price = 0.02 ether;
+        vm.prank(alice); // Set the contract caller to Alice
+        marketplace.listReservation(tokenId, price);
+
+        // Purchase the reservation
+        uint256 startBalance = alice.balance; //4.99 ether
+        vm.prank(bob);
+        marketplace.buyReservation{value: price}(tokenId); // alice gets 0.02 so 5.01 bob has 4.98
+
+        // Withdraw the funds
+
+        vm.prank(alice);
+        marketplace.withdrawProceeds();
+
+        uint256 endBalance = alice.balance;
+        assertEq(endBalance, startBalance + price);
+
+        // Check new owner
+        address tokenOwner = reservNFT.ownerOf(tokenId);
+        assertEq(tokenOwner, bob); // Check if the new owner is bob
+    }
+
+    function test_removeReservationListing() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400; // January 10, 2023 21:00:00 UTC
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        // Approve Marketplace to manage the token
+        reservNFT.approve(address(marketplace), tokenId);
+
+        // List the reservation
+        uint256 price = 0.02 ether;
+        marketplace.listReservation(tokenId, price);
+
+        // Remove the reservation listing
+        marketplace.cancelReservationListing(tokenId);
+
+        // Check if the listing has been removed
+        Marketplace.Listing memory listing = marketplace.getReservationListing(
+            tokenId
+        );
+    }
+
+    function testRevertWhen_listReservation_invalidPrice() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400;
+
+        address alice = vm.addr(1);
+        payable(alice).transfer(5 ether);
+        vm.prank(alice);
+
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        vm.prank(alice);
+
+        reservNFT.approve(address(marketplace), tokenId);
+
+        vm.expectRevert(Marketplace__PriceMustBeAboveZero.selector);
+        vm.prank(alice);
+
+        marketplace.listReservation(tokenId, 0);
+    }
+
+    function testRevertWhen_listReservation_notApproved() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400;
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        vm.expectRevert(Marketplace__NotApprovedForMarketplace.selector);
+        marketplace.listReservation(tokenId, 0.02 ether);
+    }
+
+    function testRevertWhen_buyReservation_insufficientFunds() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400;
+
+        address alice = vm.addr(1);
+        payable(alice).transfer(5 ether);
+        vm.prank(alice);
+
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        vm.prank(alice);
+        reservNFT.approve(address(marketplace), tokenId);
+        vm.prank(alice);
+        marketplace.listReservation(tokenId, 0.02 ether);
+
+        bytes memory expectedError = abi.encodeWithSignature(
+            "Marketplace__PriceNotMet(uint256,uint256)",
+            tokenId,
+            0.02 ether
+        );
+        vm.expectRevert(expectedError);
+
+        marketplace.buyReservation{value: 0.001 ether}(tokenId);
+    }
+
+    function testRevertWhen_cancelReservationListing_notOwner() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400;
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        reservNFT.approve(address(marketplace), tokenId);
+        marketplace.listReservation(tokenId, 0.02 ether);
+
+        address notOwner = vm.addr(1);
+        vm.prank(notOwner);
+        vm.expectRevert(Marketplace__NotOwner.selector);
+        marketplace.cancelReservationListing(tokenId);
+    }
+
+    function testRevertWhen_updateReservationListing_notOwner() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400;
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        reservNFT.approve(address(marketplace), tokenId);
+        marketplace.listReservation(tokenId, 0.02 ether);
+
+        address notOwner = vm.addr(1);
+        vm.prank(notOwner);
+        vm.expectRevert(Marketplace__NotOwner.selector);
+        marketplace.updateReservationListing(tokenId, 0.03 ether);
+    }
+
+    function testRevertWhen_withdrawProceeds_noProceeds() public {
+        address addr = vm.addr(1);
+        vm.prank(addr);
+
+        vm.expectRevert(Marketplace__NoProceeds.selector);
+        marketplace.withdrawProceeds();
+    }
+
+    function test_listCancelAndRelistReservation() public {
+        uint256 dropId = 0;
+        uint256 reservationTimestamp = 1673384400; // January 10, 2023 21:00:00 UTC
+        uint256 tokenId = _createReservNFT(
+            dropId,
+            reservationTimestamp,
+            0.01 ether
+        );
+
+        // Approve the marketplace to handle the NFT
+        reservNFT.approve(address(marketplace), tokenId);
+
+        uint256 price = 0.1 ether;
+        marketplace.listReservation(tokenId, price);
+
+        // Test if the listing was successful
+        Marketplace.Listing memory listing = marketplace.getReservationListing(
+            tokenId
+        );
+        assertEq(listing.seller, address(this));
+        assertEq(listing.price, price);
+
+        // Test canceling the reservation listing
+        marketplace.cancelReservationListing(tokenId);
+
+        // Test if the listing was removed
+        listing = marketplace.getReservationListing(tokenId);
+        assertEq(listing.price, 0);
+
+        // Test relisting the reservation
+        uint256 newPrice = 0.15 ether;
+        marketplace.listReservation(tokenId, newPrice);
+
+        // Test if the relisting was successful
+        listing = marketplace.getReservationListing(tokenId);
+        assertEq(listing.seller, address(this));
+        assertEq(listing.price, newPrice);
+    }
+}
